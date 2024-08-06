f67ac22ce300d24adc9c54455e59497de7f97a82
ShinraTenseiRe <avidewan05@gmail.com>
2024-08-06 09:53:15 +0600

    fix

--- data/simCLRloader.py
+++ data/simCLRloader.py
@@ -48,7 +48,6 @@ class SimCLRDataset(data.Dataset):
                         transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)
                     ], p=0.8),
                     transforms.RandomGrayscale(p=0.2),
-                    transforms.RandomRotation(90),
                     transforms.GaussianBlur(kernel_size=9),
                     transforms.ToTensor(),
                     transforms.Normalize(self.mean_pix, self.std_pix)
