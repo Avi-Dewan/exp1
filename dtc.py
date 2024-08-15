9e7c00545027d17f774b0c0cbf70179b815dba58
ShinraTenseiRe <avidewan05@gmail.com>
2024-08-15 17:29:29 +0600

    done

--- dtc.py
+++ dtc.py
@@ -269,9 +269,8 @@ if __name__ == "__main__":
     print(model)
     print('---------------------------------')
     for name, param in model.named_parameters(): 
-        # if 'linear' not in name:
-        #     param.requires_grad = False
-        print(name)
+        if 'linear' not in name and 'layer3' not in name and 'layer4' not in name:
+            param.requires_grad = False
 
     warmup_train(model, train_loader, eval_loader, args)
     if args.DTC == 'Baseline':
